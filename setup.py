a8014abe07bd2b04526a69a959b57651dece99d4
maelic <teoneau@gmail.com>
2023-03-05 17:05:32 +1030

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sg2im',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      packages=['sg2im'],
+     )
